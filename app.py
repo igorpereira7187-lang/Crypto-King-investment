2027f9e7e3611680b44a28222a4856640e7f4653
igorpereira7187-lang <igorpereira7187@gmail.com>
2025-12-27 11:51:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "💰 SITE DE INVESTIMENTOS ATIVO 💰"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
